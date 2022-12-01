22906bedbbff09d92a8915e158c7fca1c1b6d043
aaron <aaronhensley92@gmail.com>
2022-11-30 19:26:05 -0600

    Did practice with modifying text and styles in js

--- vscode/app.js
+++ vscode/app.js
@@ -1,7 +1,15 @@
-var text = document.querySelector("para").textContent
-document.querySelector("head").textContent = text
+var text = document.querySelector("#head").textContent
+document.querySelector("#para").textContent = text
+
+document.querySelector("#notstuff").textContent = text
+
+head.style.color = "red"
+
+notstuff.style.padding = "50px"
+
+document.body.style.backgroundColor = "burlywood"
 
 var myBtn = document.getElementById("head")
 myBtn.addEventListener("click" , function(){
-alert("No")
+alert("No!")
 })
