37a1a5402cc4cb7cc1c975b4398c246c139f37c9
aaron <aaronhensley92@gmail.com>
2022-12-01 21:20:24 -0600

    Did lots more js practice by creating HTML elements with JS

--- vscode/app.js
+++ vscode/app.js
@@ -1,15 +1,28 @@
-var text = document.querySelector("#head").textContent
-document.querySelector("#para").textContent = text
+// var text = document.querySelector("#head").textContent
+// document.querySelector("#para").textContent = text
 
-document.querySelector("#notstuff").textContent = text
+// document.querySelector("#notstuff").textContent = text
 
-head.style.color = "red"
+// head.style.color = "red"
 
-notstuff.style.padding = "50px"
+// notstuff.style.padding = "50px"
 
-document.body.style.backgroundColor = "burlywood"
+// document.body.style.backgroundColor = "burlywood"
 
 var myBtn = document.getElementById("head")
 myBtn.addEventListener("click" , function(){
 alert("No!")
 })
+
+
+
+var newLi = document.createElement("li")
+var stuff = document.getElementById("stuff")
+stuff.prepend(newLi)
+newLi.textContent = "Table"
+
+var newP = document.createElement("p")
+document.body.prepend(newP)
+newP.textContent = "Did it work?"
+newP.style.fontSize = "60px"
+newP.style.textAlign = ("center")
