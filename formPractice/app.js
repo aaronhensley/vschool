301f5e8bd380146d95ccfe4c691e76409cdc2ec8
aaron <aaronhensley92@gmail.com>
2022-12-20 13:59:43 -0600

    Finished form practice and will be used as notes for later

--- formPractice/app.js
+++ formPractice/app.js
@@ -1,13 +1,41 @@
-const form = document["my-form"]
+const form = document.myForm
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    const lastName = form.lastName.value
-    const firstName = form.firstName.value
-    form.firstName.value = ""
-    form.lastName.value = ""
-    const h1 = document.createElement('h1')
-    h1.textContent = firstName + " " + lastName
-    document.getElementsByTagName("body")[0].append(h1)
-    
+    // console.log(form.yesOrNo.value)
+    // console.log(form.entertainment)
+
+    // const checkedInputs = []
+
+    // for(let i = 0; i < form.entertainment.length; i++){
+    //     if(form.entertainment[i].checked){
+    //         checkedInputs.push(form.entertainment[i].value)
+    //     }
+    // }
+    // console.log(checkedInputs)
+
+    console.log(form.city.value)
 })
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault()
+//     const lastName = form.lastName.value
+//     const firstName = form.firstName.value
+//     form.firstName.value = ""
+//     form.lastName.value = ""
+//     const h1 = document.createElement('h1')
+//     h1.textContent = firstName + " " + lastName
+//     document.getElementsByTagName("body")[0].append(h1)
+    
+// })
+
+// const words = document.words
+// document.words.addEventListener("submit", function(event) {
+//     event.preventDefault()
+
+//     const noun = document.words.noun.value
+//     const adverb = document.words.adverb.value
+//     const verb = document.words.verb.value
+
+//     alert("The " + noun + " did " + adverb + " " + verb + " in it's home.")
+// })
