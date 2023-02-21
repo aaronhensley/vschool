5b166c55f3913956c7f69fe3ea8104c7c2706274
aaron <aaronhensley92@gmail.com>
2023-02-21 03:16:52 -0600

    Started colossal RPG

---
+++ colossal-rpg/app.js
@@ -0,0 +1,5 @@
+const readline = require("readline-sync");
+
+const name = readline.question("Hello Advneturer! What is your name?")
+
+prompt("Hello" + name)
---
+++ problem-solving/app.js
@@ -0,0 +1,23 @@
+const array = [1, 7, 84, 3, 489]
+
+let max = array[0];
+for (let i =0; i < array.length; i++){
+    if (array[i] > max) max = array[i]
+}
+console.log("max =" + max)
+
+
+const listOfStrings = ["$hello!", "%%^%%", "test!"]
+
+
+listOfStrings
+
+
+
+// for (let i =0; i <= listOfStrings.length; i++){
+//     function getH(){
+//         if (listOfStrings.includes("h")){
+//             console.log(i)
+//         }
+//     }
+// }
